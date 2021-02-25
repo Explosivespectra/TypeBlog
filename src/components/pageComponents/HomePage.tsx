20c4bcc1387e9983c097ab5271daa4e235da06d7
KyungjinJPark <kjparkcontact@gmail.com>
2021-02-25 03:09:46 -0500

    we have the meets

--- src/components/pageComponents/HomePage.tsx
+++ src/components/pageComponents/HomePage.tsx
@@ -1,21 +1,21 @@
-import {useState} from "react";
-import { Grid } from "@material-ui/core";
+import React, { useState } from "react";
+import { Container, Grid, Typography } from "@material-ui/core";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme: Theme) => createStyles({
+  homeH1: {
+    fontFamily: "Genshin",
+  }
+}));
 
 const HomePage: React.FC = () => {
+  const classes = useStyles();
   return (
-    <>
-      <Grid container direction="column" justify="flex-start" alignItems="center">
-
-        <Grid item><p style={{ fontSize: "5rem" }}>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p></Grid>
-        <Grid item><p style={{ fontSize: "5rem", fontFamily: "Genshin" }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quod quibusdam
-        placeat non ullam reprehenderit illo ex, dolorem hic aut provident doloremque velit. Alias tempore, officiis totam nemo consequatur unde
-        sequi eaque commodi aspernatur recusandae autem possimus voluptatum, quam repudiandae tenetur repellat minima atque voluptas repellendus
-        blanditiis cumque! Itaque officiis sint beatae ipsam vero exercitationem ut, repellendus praesentium maiores asperiores atque saepe illo
-        labore impedit fuga et facilis veritatis! Ea, ullam distinctio voluptatem quaerat vitae inventore est pariatur, amet qui nam natus ex.
-                Excepturi ea corporis molestiae nesciunt nobis cupiditate rem quod, fuga veritatis deserunt ratione voluptatum incidunt cum. Cumque.</p></Grid>
-      </Grid>
-    </>
+    <Container>
+      <Typography variant="h1" className={classes.homeH1}>Genshin Impact Menu</Typography>
+      <Typography variant="h2">We have the meats.</Typography>
+    </Container>
   );
-}
+};
 
 export { HomePage };
