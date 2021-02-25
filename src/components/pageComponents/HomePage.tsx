25a903142dfb746e7a7b889026cd68a64db21de1
Explosivespectra <playonly88@gmail.com>
2021-02-25 02:32:26 -0500

    Proper syntax for types

--- src/components/pageComponents/HomePage.tsx
+++ src/components/pageComponents/HomePage.tsx
@@ -1,17 +1,21 @@
-import {useState} from 'react';
+import { useState } from 'react';
+import { Grid } from '@material-ui/core';
 
 const HomePage: React.FC = () => {
-    return (
-        <div>
-            <p style={{fontSize: "5rem"}}>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-            <p style={{fontSize: "5rem", fontFamily: "Genshin"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quod quibusdam
-            placeat non ullam reprehenderit illo ex, dolorem hic aut provident doloremque velit. Alias tempore, officiis totam nemo consequatur unde
-            sequi eaque commodi aspernatur recusandae autem possimus voluptatum, quam repudiandae tenetur repellat minima atque voluptas repellendus
-            blanditiis cumque! Itaque officiis sint beatae ipsam vero exercitationem ut, repellendus praesentium maiores asperiores atque saepe illo
-            labore impedit fuga et facilis veritatis! Ea, ullam distinctio voluptatem quaerat vitae inventore est pariatur, amet qui nam natus ex.
-            Excepturi ea corporis molestiae nesciunt nobis cupiditate rem quod, fuga veritatis deserunt ratione voluptatum incidunt cum. Cumque.</p>
-        </div>
-    );
+  return (
+    <>
+      <Grid container direction="column" justify="flex-start" alignItems="center">
+
+        <Grid item><p style={{ fontSize: "5rem" }}>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p></Grid>
+        <Grid item><p style={{ fontSize: "5rem", fontFamily: "Genshin" }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quod quibusdam
+        placeat non ullam reprehenderit illo ex, dolorem hic aut provident doloremque velit. Alias tempore, officiis totam nemo consequatur unde
+        sequi eaque commodi aspernatur recusandae autem possimus voluptatum, quam repudiandae tenetur repellat minima atque voluptas repellendus
+        blanditiis cumque! Itaque officiis sint beatae ipsam vero exercitationem ut, repellendus praesentium maiores asperiores atque saepe illo
+        labore impedit fuga et facilis veritatis! Ea, ullam distinctio voluptatem quaerat vitae inventore est pariatur, amet qui nam natus ex.
+                Excepturi ea corporis molestiae nesciunt nobis cupiditate rem quod, fuga veritatis deserunt ratione voluptatum incidunt cum. Cumque.</p></Grid>
+      </Grid>
+    </>
+  );
 }
 
-export {HomePage};
+export { HomePage };
